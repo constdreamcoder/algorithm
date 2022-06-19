78908df77ca9558215ec771fd540a7fc6de2b86f
suchan <scdreamricher@gmail.com>
2022-06-19 16:28:05 +0900

    Re-solve the Backjoon's problem number 15552.

--- Backjoon/단계/반복문/15552.js
+++ Backjoon/단계/반복문/15552.js
@@ -1,11 +1,14 @@
-let input = require("fs")
-	.readFileSync("/dev/stdin")
-	.toString()
-	.trim()
-	.split("\n");
+let input = require("fs").readFileSync("/dev/stdin").toString().split("\n");
 
-for (let i = 1; i <= input[0]; i++) {
-	let numbers = input[i].split(" ");
+let max = Number(input[0]);
+let answer = "";
 
-	console.log(Number(numbers[0]) + Number(numbers[1]));
+for (let i = 1; i <= max; i++) {
+	let num = input[i].split(" ");
+	answer += Number(num[0]) + Number(num[1]) + "\n";
 }
+
+console.log(answer);
+
+// 문제에서는 하나씩 출력해도 된다고 하지만, 하나씩 출력을 하면 필히 시간 초과라는 결과를 얻었습니다.
+// 따라서 하나의 문자열에 결과값과 개행 문자를 마지막에 넣어 출력했습니다.
